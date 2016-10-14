23c20efc23f62b39b9248bcaae81eb0711e213ef
HiroyukiYahagi <hiyahagi@HIYAHAGI-JP2.jp.oracle.com>
2016-10-14 20:39:35 +0900

    コンポーネント追加

--- js/hy_template.js
+++ js/hy_template.js
@@ -1,10 +1,50 @@
-console.log("test");
-$(document).on('ready', function() {
-    $(".center").slick({
-	    dots: true,
-	    infinite: true,
-	    centerMode: true,
-	    slidesToShow: 3,
-	    slidesToScroll: 3
-    });
+var initTemplate = function (){
+	//
+	// slider
+	//
+	$(".hy-slider").each(function(){
+		var show = $(this).data('show');
+		$(this).slick({
+		    dots: true,
+		    infinite: true,
+		    centerMode: true,
+		    slidesToShow: show,
+		    slidesToScroll: 1
+	    });
+	});
+
+	//
+	// tab
+	//
+	$(".hy-tab-menu > button").addClass("inactive");
+	$(".hy-tab-menu > button:first-child").removeClass("inactive");
+	$(".hy-tab-content > *").each(function(){
+		$(this).hide();
+	});
+	$(".hy-tab-content > *:first-child").each(function(){
+		$(this).show(300);
+	});
+	$(".hy-tab-menu > button").click(function(){
+		$(".hy-tab-menu > button").addClass("inactive");	
+		$(this).removeClass("inactive");
+
+		var targetId = $(this).attr('for');
+		$(".hy-tab-content > *").hide(100);
+		$("#" + targetId).show(300);
+	});
+
+
+	//
+	// flush
+	//
+	$(".hy-flush").click(function(){
+		$(this).hide(300);
+		$(this).remove();
+	});
+
+}
+
+$(document).ready(function() {
+	initTemplate();
+
 });
